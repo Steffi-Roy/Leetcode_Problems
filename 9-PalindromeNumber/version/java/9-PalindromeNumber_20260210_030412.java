5ae15d34e240df9bbf4f26f1b6f8e14980b7b759
Steffi <38868366+Steffi-Roy@users.noreply.github.com>
2026-02-10 03:04:12 -0500

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/version/java/9-PalindromeNumber_20260210_030412.java
@@ -0,0 +1,19 @@
+// Last updated: 2/10/2026, 3:04:12 AM
+1class Solution {
+2    public boolean isPalindrome(int x) {
+3        int reversed =0;
+4        int digit =0;
+5        int n = x;
+6        if (n<0){
+7            return false;
+8        }
+9        while(n>0){
+10            digit=n%10; //121
+11            reversed = reversed * 10 + digit; //121
+12            n=n/10;
+13            //System.out.println(reversed);
+14        }
+15        
+16       return(x==reversed);
+17    }
+18}
